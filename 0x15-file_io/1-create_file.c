c1f7fe759d30ab63bc3989b80c30d5cfc4f7b951
AYouB-P-HA <hawkridge00@gmail.com>
2023-09-04 16:09:23 -0700

    task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: pointer to the name of the file
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: -1 or 1
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int op, w, num;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (num = 0; text_content[num];)
+			num++;
+	}
+
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(op, text_content, num);
+
+	if (op == -1 || w == -1)
+		return (-1);
+
+	close(op);
+	return (1);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: pointer to the name of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ **/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, w;
+    int length = 0;
+	if (filename == NULL)
+		return (-1);
+
+	op = open(filename, O_WRONLY | O_APPEND);
+    w = write(op, text_content, length);
+	if (text_content != NULL)
+	{
+		length = strlen(text_content);
+	}
+
+	if (op == -1 || w == -1)
+		return (-1);
+
+	close(op);
+	return (1);
+}
